4ef1da0ec45844c2cae8b1799eea34065940ea28
Carlos Alcca <edual986@gmail.com>
2020-10-21 16:10:57 -0300

    Close #33 Implement infraestructure MVC turns

--- resources/views/turns/index.blade.php
+++ resources/views/turns/index.blade.php
@@ -2,12 +2,12 @@
     <div class="mb-6 lg:flex lg:items-center lg:justify-between">
         <div class="flex-1 min-w-0">
             <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
-                Clientes
+                Turnos
             </h2>
         </div>
         <div class="mt-5 flex lg:mt-0 lg:ml-4">
             <span class="sm:ml-3 shadow-sm rounded-md">
-                <a href="{{ route( 'clients.create' )}}">
+                <a href="{{ route( 'turns.create' )}}">
                     <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out">
                         <svg class="-ml-1 mr-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path>
@@ -27,16 +27,13 @@
                         <thead>
                             <tr>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                    Nombre
+                                    Fecha
                                 </th>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                    Apellido
+                                    Hora
                                 </th>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                    Edad
-                                </th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                    Telefono
+                                    Estado
                                 </th>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                     Fecha alta
@@ -47,43 +44,27 @@
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach($clients as $client)
+                            @foreach($turns as $turn)
                             <tr>
                                 <td class="px-6 py-4 whitespace-no-wrap">
                                     <div class="flex items-center">
                                         <div class="ml-4">
                                             <div class="text-sm leading-5 font-medium text-gray-900">
-                                                {{ $client->name }}
+                                                {{ $turn->date }}
                                             </div>
                                         </div>
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-no-wrap">
-                                    <div class="text-sm leading-5 text-gray-900">{{ $client->lastname }}</div>
+                                    <div class="text-sm leading-5 text-gray-900">{{ $turn->time }}</div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-no-wrap">
-                                    <div class="text-sm leading-5 text-gray-900">{{ $client->age }}</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-no-wrap">
-                                    <div class="text-sm leading-5 text-gray-900">{{ $client->phone }}</div>
+                                    <div class="text-sm leading-5 text-gray-900">{{ $turn->pending }}</div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-                                    {{ $client->created_at }}
+                                    {{ $turn->created_at }}
                                 </td>
-                                <td class="px-5 py-4 flex space-x-4">
-                                    <a href=" {{ route('clients.edit',$client->id )}}" class="text-indigo-600 hover:text-indigo-900"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg></a>
-                                    <form action="{{ route('clients.destroy', $client->id )}}" method="post">
-                                        {{ method_field('DELETE') }}
-                                        {{ csrf_field() }}
-                                        <button type="submit" >
-                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                                                    </path>
-                                                </svg>
-                                            </a>
-                                        </button>
-                                    </form>    
+                                <td class="px-5 py-4 flex space-x-4">   
                                 </td>
                             </tr>
                             @endforeach
