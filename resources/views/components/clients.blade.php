ff0e31a0be73be941be5d6f39e89b9a2dc9a6326
Carlos Alcca <edual986@gmail.com>
2020-10-21 16:18:31 -0300

    'cambios no guardados, titulo del component y la edicion del test'

--- resources/views/components/clients.blade.php
+++ resources/views/components/clients.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Clientes
+            Sistema de gestión de turnos
         </h2>
     </x-slot>
 
